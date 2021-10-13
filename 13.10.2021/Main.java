88b22bdf8af039a1f2ff4a4bcf525e19b739846e
vladlen-vozhzhaev <55277829+vladlen-vozhzhaev@users.noreply.github.com>
2021-10-13 18:47:13 +0300

    Create Main.java

---
+++ 13.10.2021/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 17;
+        double num2 = 3;
+        System.out.println(num1+num2); // 20
+        System.out.println(num1-num2); // 14
+        System.out.println(num1*num2); // 51
+        System.out.println(num1/num2); // 5
+        System.out.println(num1%num2); // 2
+        System.out.println(num1>num2); // true
+        System.out.println(num1==num2);// false
+        System.out.println(num1>=num2);// true
+
+
+        // String name = "Ivan";
+        // System.out.println("Hello "+name);
+    }
+}
